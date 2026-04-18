72da2260298860a41c053f76c8e2db3b348d9f66
Shaykuhh <shaykerssens30@gmail.com>
2026-04-18 19:36:38 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
+  // Voeg "|| {}" toe om te voorkomen dat de build crasht als useSession undefined is
+  const { data: session } = useSession() || {};
 
   return (
     <div style={{ background: "#0b0f1a", color: "white", minHeight: "100vh", padding: "40px" }}>
@@ -13,7 +14,7 @@ export default function Home() {
         </button>
       ) : (
         <>
-          <p>Logged in as {session.user.name}</p>
+          <p>Logged in as {session.user?.name}</p>
           <button onClick={() => signOut()}>Logout</button>
         </>
       )}
